1a4b465e3783b63789a297011d308961319cea05
ndraaditiya <andraaries1998@gmail.com>
2023-01-30 13:14:17 +0700

    push

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,6 +1,6 @@
-import * as dotenv from 'dotenv'
+// import * as dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
-// export const hygraphApi = 'https://api-ap-northeast-1.hygraph.com/v2/cl8iofm8c014g01rt6l0t1sx3/master'
-export const hygraphApi = process.env.hygraphApiUrl
+export const hygraphApi = 'https://api-ap-northeast-1.hygraph.com/v2/cl8iofm8c014g01rt6l0t1sx3/master'
+// export const hygraphApi = process.env.hygraphApiUrl
